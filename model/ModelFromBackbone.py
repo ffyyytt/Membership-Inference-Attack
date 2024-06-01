7360008eb1c8cbdcdbe8e3fc7e1e552c416e2e0d
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-01 03:50:25 +0200

    "squeezenet1_0"

--- model/ModelFromBackbone.py
+++ model/ModelFromBackbone.py
@@ -4,7 +4,7 @@ import torchvision
 class ModelFromBackbone(torch.nn.Module):
     def __init__(self, backbone, num_classes):
         super().__init__()
-        self.backbone = getattr(torchvision.models, backbone)(weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1)
+        self.backbone = getattr(torchvision.models, backbone)(weights=None)
         self.backbone.fc = torch.nn.Linear(self.backbone.fc.in_features, num_classes)
 
     def forward(self, x):
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 from model.ModelFromBackbone import *
 from model.unit import *
 
-def trainModel(dataLoader, device, n_classes):
-    model = ModelFromBackbone("resnet18", n_classes).to(device)
+def trainModel(dataLoader, device, n_classes, backbone = "squeezenet1_0"):
+    model = ModelFromBackbone(backbone, n_classes).to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-3)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
     loss_fn=torch.nn.CrossEntropyLoss().to(device)
