0c5ea34056b866b62a69bfbfe4429bf10d0aff96
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-01 04:05:04 +0200

    Update ModelFromBackbone.py

--- model/ModelFromBackbone.py
+++ model/ModelFromBackbone.py
@@ -9,7 +9,7 @@ class ModelFromBackbone(torch.nn.Module):
         if hasattr(self.backbone, 'fc'):
             self.backbone.fc = torch.nn.Linear(self.backbone.fc.in_features, num_classes)
         elif hasattr(self.backbone, 'classifier') and isinstance(self.backbone.classifier, torch.nn.Sequential):
-            self.backbone.classifier = torch.nn.Linear(self.backbone.classifier.in_features, num_classes)
+            self.backbone.classifier = torch.nn.Linear(self.backbone.last_channel, num_classes)
         else:
             raise NotImplementedError(f"Backbone {backbone} is not supported yet.")
 
