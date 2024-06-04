217ba02d0a37c675a19dcaea4f9b0e26180979b9
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-04 13:47:10 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ from model.unit import *
 
 def trainModel(dataLoader, device, n_classes, backbone = "mobilenet_v2"):
     model = ModelFromBackbone(backbone, n_classes).to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=5e-2, momentum=0.9, weight_decay=1e-3)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=1e-3)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
     loss_fn=torch.nn.CrossEntropyLoss().to(device)
 
     train_unit = MyTrainUnit(module=model, optimizer=optimizer, lr_scheduler=scheduler, loss_fn=loss_fn, totalSteps=len(dataLoader))
-    torchtnt.framework.train(train_unit, dataLoader, max_epochs=20)
+    torchtnt.framework.train(train_unit, dataLoader, max_epochs=50)
     return model
 
 def modelPredict(model, dataLoader, device):
