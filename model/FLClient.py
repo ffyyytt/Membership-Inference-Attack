c3b63200bc7b1746d9ff24e33a70418132a84a58
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-05 14:56:49 +0200

    Update FLClient.py

--- model/FLClient.py
+++ model/FLClient.py
@@ -30,29 +30,29 @@ class FlowerClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         print(f"[Client {self.cid}] fit, config: {config}")
         FLset_parameters(self.net, parameters)
-        __FLtrainModelWithModel(self.net, self.trainloader, self.device , epochs=self.localEpochs)
+        self.__FLtrainModelWithModel(self.net, self.trainloader, self.device , epochs=self.localEpochs)
         return FLget_parameters(self.net), len(self.trainloader), {}
 
     def evaluate(self, parameters, config):
         print(f"[Client {self.cid}] evaluate, config: {config}")
         FLset_parameters(self.net, parameters)
-        output, label = __FLmodelPredict(self.net, self.valloader)
+        output, label = self.__FLmodelPredict(self.net, self.valloader)
         loss = torch.nn.functional.mse_loss(output, label)
         accuracy = np.mean(np.argmax(output, axis=1)==np.argmax(label, axis=1))
         return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
     
-def __FLtrainModelWithModel(model, dataLoader, device, epochs):
-    model = model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=1e-3)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-    loss_fn=torch.nn.CrossEntropyLoss().to(device)
+    def __FLtrainModelWithModel(self, model, dataLoader, device, epochs):
+        model = model.to(device)
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=1e-3)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+        loss_fn=torch.nn.CrossEntropyLoss().to(device)
 
-    train_unit = MyTrainUnit(module=model, optimizer=optimizer, lr_scheduler=scheduler, loss_fn=loss_fn, totalSteps=len(dataLoader))
-    torchtnt.framework.train(train_unit, dataLoader, max_epochs=epochs)
-    return model
+        train_unit = MyTrainUnit(module=model, optimizer=optimizer, lr_scheduler=scheduler, loss_fn=loss_fn, totalSteps=len(dataLoader))
+        torchtnt.framework.train(train_unit, dataLoader, max_epochs=epochs)
+        return model
 
-def __FLmodelPredict(model, dataLoader, device):
-    model = model.to(device)
-    predUnit = MyPredictUnit(module=model)
-    torchtnt.framework.predict(predUnit, dataLoader)
-    return predUnit.outputs, predUnit.labels
+    def __FLmodelPredict(self, model, dataLoader, device):
+        model = model.to(device)
+        predUnit = MyPredictUnit(module=model)
+        torchtnt.framework.predict(predUnit, dataLoader)
+        return predUnit.outputs, predUnit.labels
