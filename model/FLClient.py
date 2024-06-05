b0aeb1334b9dda6ab1250a6845d0bcf2f99ac01c
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-05 15:16:35 +0200

    Update FLClient.py

--- model/FLClient.py
+++ model/FLClient.py
@@ -13,8 +13,8 @@ def FLget_parameters(net) -> List[np.ndarray]:
 
 def FLset_parameters(net, parameters: List[np.ndarray]):
     params_dict = zip(net.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-    # net.load_state_dict(state_dict, strict=True)
+    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict if len(v.shape)>0})
+    net.load_state_dict(state_dict, strict=True)
 
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self, cid, net, device, trainloader, valloader, localEpochs):
