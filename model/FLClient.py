4374dc9a85385fef8cf32185e1a106c060585bda
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-05 14:44:44 +0200

    Update

--- model/FLClient.py
+++ model/FLClient.py
@@ -10,7 +10,6 @@ def FLget_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
 def FLset_parameters(net, parameters: List[np.ndarray]):
-    print(parameters)
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
