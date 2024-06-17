0776fefbd1d1ed43d914d8e6ce999912c4642fad
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-17 14:12:31 +0200

    Update main_cen.py

--- main_cen.py
+++ main_cen.py
@@ -11,7 +11,7 @@ miaDataLoader, memberLabels, inOutLabels = dataCIFARonline.loadMIADataCIFAR10(de
 cenModel = trainModel(cenTrainDataLoader, device, dataCIFARonline.__CIFAR10_N_CLASSES__, backbone)
 yPred = modelPredict(cenModel, miaDataLoader, device)
 
-print(yPred[0].shape)
+print(np.mean(np.argmax(yPred[0], axis=1) == np.argmax(yPred[1], axis=1)))
 
 # shadowPreds = []
 # shadowModels = []
