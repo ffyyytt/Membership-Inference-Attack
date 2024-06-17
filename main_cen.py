3376c8f9594c8c8bbafcc5e7457e7900f9807c6f
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-18 01:56:01 +0200

    Update main_cen.py

--- main_cen.py
+++ main_cen.py
@@ -2,10 +2,23 @@ import torch
 import dataCIFARonline
 from utils import *
 from sklearn.metrics import roc_auc_score
+import torchvision.transforms as transforms
 
 backbone = "resnet18"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-cenTrainDataLoader = dataCIFARonline.loadCenTrainCIFAR10(device)
+# cenTrainDataLoader = dataCIFARonline.loadCenTrainCIFAR10(device)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 32
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+cenTrainDataLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
 miaDataLoader, memberLabels, inOutLabels = dataCIFARonline.loadMIADataCIFAR10(device)
 
 cenModel = trainModel(cenTrainDataLoader, device, dataCIFARonline.__CIFAR10_N_CLASSES__, backbone)
