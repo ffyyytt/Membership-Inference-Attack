0899b4c880a83fcfb4cf7e5601940789943fef04
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-06-27 15:29:19 +0200

    Update run_tf.sh

--- run_tf.sh
+++ run_tf.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres gpu:2
 #SBATCH --time 6-23:50:00
 #SBATCH --cpus-per-task 18
-#SBATCH --mem-per-cpu 3G
+#SBATCH --mem-per-cpu 5G
 #SBATCH --mail-type FAIL,END
 
 conda activate ffyytt
